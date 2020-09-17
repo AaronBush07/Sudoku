a4df9ee1c87b16ea9ec62cf7dfd8537040b6f919
Aaronbush07 <Aaron.bush07@gmail.com>
2020-09-17 21:27:08 +1000

    Implemented basic game board

---
+++ src/components/Footer.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+      <footer class="Footer"><p>Created in Vue.js by Aaron Bush.</p> </footer>
+  </div>
+</template>
+
+<script>
+export default {
+    name: 'Footer'
+}
+</script>
+
+<style scoped>
+.Footer {
+    left: 0;
+    bottom: 0;
+    width: 100%;
+    text-align: center;
+    }
+</style>
---
+++ src/components/GameBoard.vue
@@ -0,0 +1,42 @@
+<template>
+  <div>
+      <table class="sudokuTable">
+          <tbody>
+            <tr v-for="index in 9" :key="index">
+                <td v-for="cell in 9" :key="cell"><input type="text" class="Cell" /></td>
+            </tr>
+          </tbody>
+      </table>
+  </div>
+</template>
+
+<script>
+export default {
+}
+</script>
+
+<style scoped>
+  .sudokuTable {
+    margin-left: auto;
+    margin-right: auto;
+    border-collapse: collapse;
+    border: 2px solid;
+  }
+  td {
+      border: 1px solid;
+  }
+  .Cell {
+    border: None;
+    text-align: center;
+    height: 50px;
+    width: 50px;
+  }
+  td:nth-child(3),td:nth-child(6)
+  {
+      border-right: 2px solid;
+  }
+  tr:nth-child(3),tr:nth-child(6)
+  {
+      border-bottom: 2px solid;
+  }
+</style>
